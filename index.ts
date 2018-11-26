8b207b67b6557c095b253b6e018c3aaf400dce0b
Daniel Słaby <pufek93@gmail.com>
2018-11-26 09:25:16 +0100

    Update syntax

--- index.ts
+++ index.ts
@@ -2,33 +2,33 @@ import * as request from 'request'
 
 interface IOptions {
   interval?: number,
-  logger?: any,
+  logger?: (arg: string) => any,
   verbose?: boolean
 }
 
 export default (url?: string, options: IOptions = {}): boolean | NodeJS.Timeout => {
 
-  options.interval = options.interval || 20 * 1000 * 60;
-  options.logger = options.logger || console.log;
-  options.verbose = options.verbose || false;
-
-  var isHeroku = require("is-heroku");
-
   if (!url) {
     options.verbose && options.logger("heroku-self-ping: no url provided. Exiting.");
     return false;
   }
+
+  options.interval = options.interval || 20 * 1000 * 60;
+  options.logger = options.logger || console.log;
+  options.verbose = options.verbose || false;
+
+  const isHeroku = require('is-heroku')
+
   if (!isHeroku) {
     options.verbose && options.logger("heroku-self-ping: heroku not detected. Exiting.");
 
     return false;
   }
 
+  options.verbose && options.logger(`heroku-self-ping: Setting up heartbeat to ${url} every ${options.interval} ms.`);
 
-  options.verbose && options.logger("heroku-self-ping: Setting up heartbeat to " + url + " every " + options.interval + "ms.");
-
-  return setInterval(function () {
-    options.logger("heroku-self-ping: Sending heartbeat to " + url);
+  return setInterval(() => {
+    options.logger(`heroku-self-ping: Sending heartbeat to ${url}`);
     request(url, function () { });
   }, options.interval);
 };
